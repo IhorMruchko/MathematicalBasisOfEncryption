6be80c6461c7f16a632ff4e78d5a7875b2afefe0
Ihor Mruchko <55592478+IhorMruchko@users.noreply.github.com>
2022-09-22 21:16:27 +0300

    Update testing

--- MathematicalBasisOfEncryptionTest/DeffieHelmanCipherTesting.cs
+++ MathematicalBasisOfEncryptionTest/DeffieHelmanCipherTesting.cs
@@ -1,46 +1,41 @@
-﻿namespace MathematicalBasisOfEncryptionTest
+﻿namespace MathematicalBasisOfEncryptionTest;
+
+[TestClass]
+public class DeffieHelmanCipherTesting
 {
-    using MathematicalBasisOfEncryption.Cipheres;
-    using MathematicalBasisOfEncryption.Extensions;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    [TestClass]
-    public class DeffieHelmanCipherTesting
+    [TestMethod]
+    [DataRow(7, 3)]
+    [DataRow(23, 5)]
+    [DataRow(107, 5)]
+    [DataRow(911, 17)]
+    public void PrimeRootCheck_RightResult(long baseValue, long primitiveRoot) 
     {
-        [TestMethod]
-        [DataRow(7, 3)]
-        [DataRow(23, 5)]
-        [DataRow(107, 5)]
-        [DataRow(911, 17)]
-        public void PrimeRootCheck_RightResult(long baseValue, long primitiveRoot) 
-        {
-            var result = baseValue.HasPrimitiveRoot(primitiveRoot);
+        var result = baseValue.HasPrimitiveRoot(primitiveRoot);
 
-            Assert.IsTrue(result);
-        }
+        Assert.IsTrue(result);
+    }
 
-        [TestMethod]
-        [DataRow(7, 6)]
-        [DataRow(23, 12)]
-        [DataRow(107, 53)]
-        [DataRow(911, 107)]
-        public void PrimeRootCheck_WrongResult(long baseValue, long primitiveRoot)
-        {
-            var result = baseValue.HasPrimitiveRoot(primitiveRoot);
+    [TestMethod]
+    [DataRow(7, 6)]
+    [DataRow(23, 12)]
+    [DataRow(107, 53)]
+    [DataRow(911, 107)]
+    public void PrimeRootCheck_WrongResult(long baseValue, long primitiveRoot)
+    {
+        var result = baseValue.HasPrimitiveRoot(primitiveRoot);
 
-            Assert.IsFalse(result);
-        }
+        Assert.IsFalse(result);
+    }
 
-        [TestMethod]
-        [DataRow(23, 5, 4, 3, 18)]
-        [DataRow(23, 5, 6, 15, 2)]
-        public void DiffiHelmanCipher_SecretKeyGeneration_Valid(long p, long q, long a, long b, long res)
-        {
-            var diffiHellmanCipher = new DiffieHellmanCipher();
+    [TestMethod]
+    [DataRow(23, 5, 4, 3, 18)]
+    [DataRow(23, 5, 6, 15, 2)]
+    public void DiffiHelmanCipher_SecretKeyGeneration_Valid(long p, long q, long a, long b, long res)
+    {
+        var diffiHellmanCipher = new DiffieHellmanCipher();
 
-            diffiHellmanCipher.SetKey((p, q, a, b));
+        diffiHellmanCipher.SetKey((p, q, a, b));
 
-            Assert.AreEqual(res, diffiHellmanCipher.SecretKey);
-        }
+        Assert.AreEqual(res, diffiHellmanCipher.SecretKey);
     }
 }
